7c8c6de1af50079bec29a96eea3b7855fcb5c598
Thorkil <schjodt.thorkil@gmail.com>
2021-12-19 17:03:00 +0100

    #build_tree working

--- lib/main.rb
+++ lib/main.rb
@@ -3,8 +3,11 @@
 require_relative 'node'
 require_relative 'tree'
 
-tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-p tree
+tree
 
 tree.build_tree
+
+p tree.root.data
+
--- lib/tree.rb
+++ lib/tree.rb
@@ -2,13 +2,32 @@
 
 # building and managing tree of nodes
 class Tree
-  attr_accessor(:array)
+  attr_accessor(:array, :root)
 
   def initialize(array = [])
     @array = array.sort
+    @root = build_tree(array)
   end
 
-  def build_tree
-    p array
+  # build binary tree recursively
+  def build_tree(array = @array)
+    return array if array.length <= 1
+
+    root = mid(array)
+    array_left = left(array)
+    array_right = right(array)
+    Node.new(root, build_tree(array_left), build_tree(array_right))
+  end
+
+  def mid(array)
+    array[(0 + array.length - 1) / 2]
+  end
+
+  def left(array)
+    array[0, (0 + array.length - 1) / 2]
+  end
+
+  def right(array)
+    array[(0 + array.length - 1) / 2 + 1, array.length - 1]
   end
 end
